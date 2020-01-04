dbdae9644a7cb24d8a50419877fe2361b923f389
willys_tjiawi <willys@huubap.com>
2020-01-05 01:44:34 +0800

    completed test case for inbox.sol

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,6 +7,7 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INIT_MESSAGE = 'Learn Solidity!';
 
 beforeEach(async() => {
     // get a list of all accounts
@@ -14,12 +15,24 @@ beforeEach(async() => {
 
     // use one of the accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Learn Solidity!'] })
+    .deploy({ data: bytecode, arguments: [INIT_MESSAGE] })
     .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () => {
+    it('deploys a contract', () => {        // check if the contract is properly deployed (by checking the deployed destination address)
         console.log('deployed inbox..... ', inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async() => {        // check if the is init message
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INIT_MESSAGE);
+    });
+
+    it('can change the message', async() => {       // test setMessage and retrieve message
+        await inbox.methods.setMessage('Create SmartContract!').send({ from: accounts[0], gas: '1000000' });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Create SmartContract!');
     });
 });
