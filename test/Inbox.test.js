ccf0cc0ef512aa360b1e7337d510ad50bbe72d2c
willys_tjiawi <willys@huubap.com>
2020-01-05 01:21:00 +0800

    test deploying contract to eth local test network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,16 +3,23 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
-    web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-        console.log('fetched Accounts..... ', fetchedAccounts);
-    });
+let accounts;
+let inbox;
+
+beforeEach(async() => {
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // use one of the accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Learn Solidity!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        
+        console.log('deployed inbox..... ', inbox);
     });
 });
