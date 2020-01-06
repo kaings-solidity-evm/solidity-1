1c49db566e6f7ec72952ae8e2f0088502a2a77b8
gr33ngr33n <actfun07@gmail.com>
2020-01-06 18:36:03 +0800

    example @truffle/hdwallet-provider failed

--- deploy.js
+++ deploy.js
@@ -1,13 +1,22 @@
 const { networks } = require('./truffle-config');
-// const { interface, bytecode } = require('./compile');
+const { interface, bytecode } = require('./compile');
 const Web3 = require('web3');
 
 const provider = networks.rinkeby.provider();
 const web3 = new Web3(provider);
 
 let accounts;
-const getAccounts = async() => {
+const INIT_MESSAGE = 'Deploy Smart Contract!';
+
+const deploy = async() => {
     accounts = await web3.eth.getAccounts();
     console.log('getAccounts..... ', accounts);     // correct connection should give your rinkeby accounts tied to your mnemonic
+
+    const inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INIT_MESSAGE] })
+    .send({ from: accounts[0] , gas: '1000000' });
+
+    console.log('deployed contract..... ', inbox);
 };
-getAccounts();
+
+deploy();
