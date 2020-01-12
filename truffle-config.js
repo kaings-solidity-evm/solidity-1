8be94dd363a79f29608431af00a5df6faafa7053
gr33ngr33n <actfun07@gmail.com>
2020-01-12 18:02:59 +0800

    refactor: move apiKey to .gitignore

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,11 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const mnemonic = require('./secret/mnemonic');
+const apiKey = require('./secret/apiKey');
 
 module.exports = {
     networks: {
         rinkeby: {
-            provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/eb6fda480d8340ffae7710c91d3c123e')
+            provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${apiKey}`)
         }
     }
 };
